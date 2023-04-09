a435186d256bba135a886d4e07634b5b83c07480
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2023-04-09 19:26:13 -0300

    NPC Controller, needing an AI Navigation Package

---
+++ Assets/Scripts/npcController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class npcController : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    public Animator animator;
+
+    public GameObject Point;
+    private Transform[] WalkingPoints;
+
+    public int index = 0;
+
+    public float minDistance = 1; 
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        WalkingPoints = new Transform[Point.transform.childCount];
+        for (int i = 0; i < WalkingPoints.Length; i++)
+        {
+            WalkingPoints[i] = Point.transform.GetChild(i); 
+        }
+    }
+
+    void Update()
+    {
+        roam();
+    }
+
+    void roam()
+    {
+        if (Vector3.Distance(transform.position ,WalkingPoints[index].position) < minDistance)
+        {
+            if (index > 0 && index < WalkingPoints.Length)
+            {
+                index += 1;
+            }
+            else 
+            {
+                index = 0;
+            }
+               
+        }
+          
+
+        agent.SetDestination(WalkingPoints[index].position); 
+
+        animator.SetFloat("Vertical" , !agent.isStopped ? 1 :0);
+    }
+}
