d1deef5b9c107a20807f47d6be0476ed387e5593
Jeremias_Abulafia <101568757+jereabu@users.noreply.github.com>
2023-06-12 09:18:01 -0300

    Fixed UI errors
    Jere = Brainless
    
    Co-Authored-By: Thiago <101569153+ThiagoBarraza@users.noreply.github.com>

--- Assets/Scripts/UI SCRIPTS/CarIndicator.cs
+++ Assets/Scripts/UI SCRIPTS/CarIndicator.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class CarIndicator : MonoBehaviour
 {
     Controller controller;
-    public Text balizas;
-    public Text guinoDer;
-    public Text cambio;
-    public Text guinoIzq;
-    public Text Vel;
+    public TMP_Text balizas;
+    public TMP_Text guinoDer;
+    public TMP_Text cambio;
+    public TMP_Text guinoIzq;
+    public TMP_Text Vel;
 void Start()
 {
-    balizas = GameObject.FindGameObjectWithTag("balizas").GetComponent<Text>(); 
-    guinoDer = GameObject.FindGameObjectWithTag("guinoDer").GetComponent<Text>(); 
-    guinoIzq = GameObject.FindGameObjectWithTag("guinoIzq").GetComponent<Text>(); 
-    Vel = GameObject.FindGameObjectWithTag("Vel").GetComponent<Text>(); 
-    cambio = GameObject.FindGameObjectWithTag("cambio").GetComponent<Text>();
+    controller = FindObjectOfType<Controller>();
+        
+    balizas = GameObject.FindGameObjectWithTag("balizas").GetComponent<TMP_Text>(); 
+    guinoDer = GameObject.FindGameObjectWithTag("guinoDer").GetComponent<TMP_Text>(); 
+    guinoIzq = GameObject.FindGameObjectWithTag("guinoIzq").GetComponent<TMP_Text>(); 
+    Vel = GameObject.FindGameObjectWithTag("Vel").GetComponent<TMP_Text>(); 
+    cambio = GameObject.FindGameObjectWithTag("cambio").GetComponent<TMP_Text>();
 }
 void Update()
 {
