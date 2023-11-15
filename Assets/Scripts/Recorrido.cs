cbafd268a50b856c374a61c17a1471ec4050b4d8
Thiago <46645966@est.ort.edu.ar>
2023-11-15 12:04:30 -0300

    Workingonlevel3

--- Assets/Scripts/Recorrido.cs
+++ Assets/Scripts/Recorrido.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Recorrido : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform Path;
+
+    [SerializeField] private List<Transform> Waypoints;
+
+    private int CurrentNode = 0;
+
     void Start()
     {
-        
+        Transform[] WaypointsTrfsm = Path.GetComponentsInChildren<Transform>();
+        Waypoints = new List<Transform>();
+
+        for (int i = 0; i < WaypointsTrfsm.Length; i++)
+        {
+            if (WaypointsTrfsm[i] != Path.transform)
+            {
+                Waypoints.Add(WaypointsTrfsm[i]);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void CheckWaypointDistance()
+    {
+        if (Vector3.Distance(transform.position, Waypoints[CurrentNode].position) < 0.5f)
+        {
+            if (CurrentNode == Waypoints.Count - 1)
+            {
+                CurrentNode = 0;
+            }
+            else
+            {
+                CurrentNode++;
+            }
+        }
     }
 }
